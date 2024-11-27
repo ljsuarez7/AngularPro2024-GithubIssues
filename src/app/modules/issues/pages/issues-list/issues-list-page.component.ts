0ee7606bc893ec86fc72b74c387d8b617d32da8f
JaviSrz <ljsuarez7@gmail.com>
2024-11-27 19:45:40 +0100

    Creación de rutas basicas y paginas basicas

---
+++ src/app/modules/issues/pages/issues-list/issues-list-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-issues-list-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './issues-list-page.component.html',
+})
+export default class IssuesListPageComponent { }
